b3d4ca09c415c4510c39b70551a1f2d1c2b585fa
fight5 <94685815+fight5@users.noreply.github.com>
2023-05-08 23:58:30 +0200

    Update Main.scala
    Add my updated code

--- data-engineering/tp-data-processing-framework/src/main/scala/com/github/polomarcus/main/Main.scala
+++ data-engineering/tp-data-processing-framework/src/main/scala/com/github/polomarcus/main/Main.scala
@@ -19,16 +19,16 @@ object Main {
     // Read a JSON data source with the path "./data-news-json"
     // Tips : https://spark.apache.org/docs/latest/sql-data-sources-json.html
     val pathToJsonData = "./data-news-json/"
-    val newsDataframe: DataFrame = ??? //@TODO
+    val newsDataframe: DataFrame = spark.read.json(pathToJsonData)
 
     // To type our dataframe as News, we can use the Dataset API : https://spark.apache.org/docs/latest/sql-getting-started.html#creating-datasets
     val newsDatasets: Dataset[News] = NewsService.read(pathToJsonData)
 
     // print the dataset schema - tips : https://spark.apache.org/docs/latest/sql-getting-started.html#untyped-dataset-operations-aka-dataframe-operations
-    //@TODO newsDatasets.???
+    newsDatasets.printSchema()
 
     // Show the first 10 elements - tips : https://spark.apache.org/docs/latest/sql-getting-started.html#creating-dataframes
-    //@TODO newsDatasets.???
+    newsDatasets.show(10)
 
     // Enrich the dataset by apply the ClimateService.isClimateRelated function to the title and the description of a news
     // a assign this value to the "containsWordGlobalWarming" attribute
@@ -37,20 +37,23 @@ object Main {
     // From now, we'll use only the Dataset API as it's more convenient
     val filteredNewsAboutClimate = NewsService.filterNews(enrichedDataset)
     // Count how many tv news we have in our data source
-    val count = NewsService.getNumberOfNews(newsDatasets)
+    val count = NewsService.getNumberOfNews(enrichedDataset)
     logger.info(s"We have ${count} news in our dataset")
 
 
     // Show how many news we have talking about climate change compare to others news (not related climate)
     // Tips: use a groupBy
-
+    enrichedDataset.groupBy($"containsWordGlobalWarming").count().show()
 
     // Use SQL to query a "news" table - look at : https://spark.apache.org/docs/latest/sql-getting-started.html#running-sql-queries-programmatically
-
+    enrichedDataset.createOrReplaceTempView("news")
+    val climateNews = spark.sql("SELECT * FROM news WHERE containsWordGlobalWarming = true")
+    climateNews.show()
 
     // Use strongly typed dataset to be sure to not introduce a typo to your SQL Query
     // Tips : https://stackoverflow.com/a/46514327/3535853
-
+    val climateNewsTyped = enrichedDataset.filter(_.containsWordGlobalWarming)
+    climateNewsTyped.show()}
 
     // Save it as a columnar format with Parquet with a partition by date and media
     // Learn about Parquet : https://spark.apache.org/docs/3.2.1/sql-data-sources-parquet.html
